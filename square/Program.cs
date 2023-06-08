91a2c68d5ecfb8dd0d8339459898be68c6e23b9a
Arman_Dzhambulov <arman1407@ya.ru>
2023-06-08 14:15:04 +0300

    square

--- square/Program.cs
+++ square/Program.cs
@@ -14,3 +14,27 @@
 // 1	            2 1 2 4 6 1	                6
 
 // 2	            0 0 0 3 3 0	                4.5
+
+Console.Clear();
+Console.Write("Введите значение x1: ");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение y1: ");
+double y1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение x2: ");
+double x2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение y2: ");
+double y2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение x3: ");
+double x3 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение y3: ");
+double y3 = double.Parse(Console.ReadLine()!);
+double square = 0;
+
+double a = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+double b = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+
+
+
+Console.WriteLine($"площадь треугольника равна: ({Math.Round (a)} * {Math.Round (b)}) % 2 = {Math.Round ((a * b) / 2.0)}");
+
+
