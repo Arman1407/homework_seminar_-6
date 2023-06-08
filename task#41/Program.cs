a2c9fd9257c430fe714ca1fdd3669102a540bdb9
Arman_Dzhambulov <arman1407@ya.ru>
2023-06-08 10:56:23 +0300

    task#41

--- task#41/Program.cs
+++ task#41/Program.cs
@@ -3,3 +3,45 @@
 
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!), positive = n, negative = 0; //, max1 = n, max2 = 0;
+while (n > 0) 
+{
+    Console.Write("Введите число: ");
+    n = int.Parse(Console.ReadLine()!);
+    if (positive < n)
+    {
+        positive = 0;
+        positive++;
+    }
+    
+    else if (negative < n)
+        positive = n;
+
+    
+      
+}
+Console.WriteLine(positive); 
+
+
+/*
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!), max1 = n, max2 = 0;
+while (n != 0) 
+{
+    Console.Write("Введите число: ");
+    n = int.Parse(Console.ReadLine()!);
+    if (max1 < n)
+    {
+        max2 = max1;
+        max1 = n;
+    }
+    else if (max2 < n)
+        max2 = n;
+}
+Console.WriteLine(max2);
+*/
