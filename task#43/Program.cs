87dd413e5ba84a805922b2f25281210581fa9ab3
Arman_Dzhambulov <arman1407@ya.ru>
2023-06-09 07:11:22 +0300

    task#43

--- task#43/Program.cs
+++ task#43/Program.cs
@@ -17,10 +17,10 @@ double k2 = double.Parse(Console.ReadLine()!);
 double x = 0;
 double y = 0;
 
-double a = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-double b = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+double a = Math.Sqrt(Math.Pow(k1 * x + b1, 2) + Math.Pow(b1, 2));
+double b = Math.Sqrt(Math.Pow(k2 * x + b2, 2) + Math.Pow(b2, 2));
 
-Console.WriteLine($"площадь треугольника равна: {Math.Round (a * b) / 2.0}");
+Console.WriteLine($"координаты точки пересечения прямых: {Math.Round (a), 2}, {Math.Round(b), 2}");
 
 
 
