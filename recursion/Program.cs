3ed09aa64ddcbbc5c17563a044df1acd10067488
Arman1407 <131908661+Arman1407@users.noreply.github.com>
2023-06-14 21:25:38 +0300

    Update Program.cs

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -24,16 +24,33 @@
 
 
 
-string[,] oil = new string[10, 10];
-
-//oil[1, 1] = "AB";
-oil[1, 1] = "BA";
-
-for (int i = 0; i < 2; i++)
+void rec(string s2, string c)
 {
-    for (int j = 0; j < 1; j++)
+    string s1;
+    int i;
+    if (s2.Length == 0)
+        Console.WriteLine(c);
+    else
     {
-        Console.WriteLine($"{oil[i, j]} ");
+        for (i = 0; i < s2.Length; i++)
+        {
+            c += s2[i];
+            s1 = s2;
+            s1 = s1.Remove(i, 1);
+            rec(s1, c);
+            c = c.Remove(c.Length - 1, 1);
+        }
     }
 }
 
+
+Console.Clear();
+string? s = Console.ReadLine(), c, s2;
+for(int i = 0; i < s.Length; i++)
+{
+    c = s[i].ToString();
+    s2 = s;
+    s2 = s2.Remove(i, 1);
+    //Console.WriteLine(s2);
+    rec(s2, c);
+}
